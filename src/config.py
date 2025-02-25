bee3eb110a571e20fd97b0ede34de09cc9c646f6
JRV7903 <jashbvora@gmail.com>
2025-02-25 19:35:22 +0530

    updating config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
-GITHUB_API_URL = "https://api.github.com"
+# GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+# GITHUB_API_URL = "https://api.github.com"
